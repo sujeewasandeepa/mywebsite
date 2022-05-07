396b37388b95e097a374759f039b19ad6eda113b
Sujeewa Sandeepa <sujeewasandeepa@protonmail.ch>
2022-05-07 17:53:48 +0530

    slider kinda finished

--- my-site/sections/Hero.js
+++ my-site/sections/Hero.js
@@ -15,16 +15,18 @@ const Hero = () => {
         If you want to contact me for some reason, give me a slide! 😉
       </p>
 
-      <div>
-        <label for="toogleButton" class="flex items-center cursor-pointer">     
-          <div class="relative">
-            <input id="toogleButton" type="checkbox" class="hidden" />
+      <div className="flex content-center">
+        <label for="togleButton" class="flex items-center cursor-pointer ml-10 my-10">     
+          <div className="relative">
+            <input id="togleButton" type="checkbox" className="hidden"/>
         
             <div
-              class="toggle-path bg-gray-200 w-40 h-10 rounded-full shadow-inner"
-            ></div>
+              className="toggle-path bg-gray-200 w-80 h-10 rounded-full shadow-inner"
+            >
+              <div className="toggle-text text-gray-600 pt-2 pl-2">sandeepasujeewa@gmail.com</div>
+            </div>
             <div
-              class="toggle-circle absolute w-5 h-5 bg-white rounded-full shadow inset-y-2.5 left-2.5"
+              className="toggle-circle absolute w-5 h-5 bg-white rounded-full shadow inset-y-2.5 left-2.5"
             ></div>
           </div>
         </label>
